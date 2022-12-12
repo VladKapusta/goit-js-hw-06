304d56ffe744b7a40e0508054039b6fcdb734567
VladKapusta <vladkapysta96@gmail.com>
2022-12-12 23:26:18 +0200

    changes  EventListener

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const inputControlRef = document.querySelector('#font-size-control');
 const spanRef = document.querySelector('#text');
 
-inputControlRef.addEventListener('click', evt => {
+inputControlRef.addEventListener('input', evt => {
   spanRef.style.fontSize = evt.currentTarget.value + 'px';
 });
